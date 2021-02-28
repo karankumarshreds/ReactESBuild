d8789a42431492b960eac86a7071ada6024f83ed
karankumarshreds <naileditnerd@gmail.com>
2021-03-01 02:16:32 +0530

    update component

--- src/components/Codeblock.tsx
+++ src/components/Codeblock.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CodeBlock: React.FC = () => {
+  const [input, setInput] = useState('');
+  // code is returned transpiled code from esbuild
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input);
+  };
+
   return (
     <div>
-      <textarea></textarea>
+      <textarea onChange={(e) => setInput(e.target.value)} value={input} />
       <div>
-        <button>Submit</button>
+        <button onClick={onClick}>Submit</button>
       </div>
       <pre></pre>
     </div>
