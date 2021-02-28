d3328ec0d652c17e074e817ba4a48744565afd35
karankumarshreds <naileditnerd@gmail.com>
2021-03-01 02:11:37 +0530

    add code block

---
+++ src/components/Codeblock.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CodeBlock: React.FC = () => {
+  return (
+    <div>
+      <textarea></textarea>
+      <div>
+        <button>Submit</button>
+      </div>
+      <pre></pre>
+    </div>
+  );
+};
+
+export default CodeBlock;
