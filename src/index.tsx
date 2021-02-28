e7c4b87e03e3d4703b4cb5bd8c8c3d6dec205de4
karankumarshreds <naileditnerd@gmail.com>
2021-03-01 02:51:03 +0530

    initialized wasm

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,37 @@
 import ReactDOM from 'react-dom';
 import CodeBlock from './components/Codeblock';
-import Codeblock from './components/Codeblock';
+
+// es build config
+import * as esbuild from 'esbuild-wasm';
+import { useEffect } from 'react';
 
 const App = () => {
+  const startService = async () => {
+    // initialize esbuild in browser
+    // returns objects of funtions used to transpile code and bundle the code
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm', // getting from public dir
+    });
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
   return <CodeBlock />;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+/**
+ * @wasm
+ * WASM means WebAssembly which provides code formats
+ * to run languages like C/C++ C#, Go, Rust etc to be
+ * run on the web browser
+ *
+ * @esbuild
+ * We are using ESBUILD-WASM module which is a JS
+ * wrapper that wraps code written in Golang which
+ * inturn will transpile and bundles code for us
+ */
